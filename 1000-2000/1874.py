9602ccfd81e30c31606ded978b96940ee0679951
unknown <yoosoobin1997@gmail.com>
2021-07-20 23:48:50 +0900

    commit

---
+++ 1000-2000/1874.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+stack = []
+result = []
+cnt = 0
+X=True
+for _ in range(n):
+    num = int(input())
+
+    while cnt < num:
+        cnt +=1
+        stack.append(cnt)
+        result.append("+")
+
+    if stack[-1] == num:
+        stack.pop()
+        result.append('-')
+
+    else:
+        X=False
+        break
+
+if X ==False:
+    print('NO')
+
+
+else:
+    for i in result:
+        print(i)
